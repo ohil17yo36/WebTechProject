ecc324a609ad4260b025cbf243556742959ae27f
Ohileshwar Itagi <ohileshwar17@gmail.com>
2015-10-20 22:32:09 +0530

    Update createDescriptor.py

--- createDescriptor.py
+++ createDescriptor.py
@@ -29,6 +29,8 @@ def height(root, depth, desList, nodes):
 height(root1, 1, desList1, nodes1)
 height(root2, 1, desList2, nodes2)
 
+#diffarray1[i] stores the difference between deslist1[i] and deslist1[i+1]
+#diffarray2[i] stores the difference between deslist2[i] and deslist2[i+1]
 for i in range(0,len(desList1)-1):
 	diffarray1.append(desList1[i]-desList1[i+1])
 
@@ -44,10 +46,12 @@ if len(desList2)<len(desList1):
 matchedTree1 = []
 matchedTree2 = []
 
+# For knowing what the other variables like k,q,pi etc are please refer to Introduction to Algorithms by Cormen page number 1005 as its just too big to explain here :P
+# Still here it goes pi[q] stores the longest length of prefix(denoted by k) of the pattern when the first q characters of the pattern match with the text
 def kmpPreCompute(pattern):
 	m=len(pattern)
-	pattern=[0]+pattern
-	pi=[0,0]
+	pattern=[0]+pattern	#I have implemented kmp on a 1 based index array. So initialy I am adding a value 0
+	pi=[0,0] #I have implemented kmp on a 1 based index array. So initialy I am adding a value 0 and pi[1]=0
 	k=0
 	for q in range(2,m+1):
 		while k>0 and pattern[k+1]!=pattern[q]:
@@ -62,8 +66,8 @@ def kmp(pattern):
 	n=len(diffarray2)
 	m=len(pattern)
 	pi=kmpPreCompute(pattern)
-	diffarray2=[0]+diffarray2
-	pattern=[0]+pattern
+	diffarray2=[0]+diffarray2 #I have implemented kmp on a 1 based index array. So initialy I am adding a value 0
+	pattern=[0]+pattern#I have implemented kmp on a 1 based index array. So initialy I am adding a value 0
 	q=0
 	for i in range(1,n+1):
 		while q>0 and pattern[q+1]!=diffarray2[i]:
@@ -71,8 +75,8 @@ def kmp(pattern):
 		if pattern[q+1]==diffarray2[i]:
 			q=q+1
 		if q==m:
-			diffarray2=diffarray2[1:i-m+1]+diffarray2[i+1:n+1]
-			matchedTree2.append(i-m)
+			diffarray2=diffarray2[1:i-m+1]+diffarray2[i+1:n+1] # The diffarray2 is spliced to exclude the part of the text that has been matched with the pattern.This is equivalent to replacing matched characters with -1.
+			matchedTree2.append(i-m)#The start index of the text which matches the pattern
 			return True
 	return False					
 
@@ -89,7 +93,7 @@ while i < len(desList1):
 		j = j + 1
 
 	if len(subTree)>1:
-		pattern=diffarray1[i:j-1]
+		pattern=diffarray1[i:j-1]# Our pattern diff array is stored in diffarray1[i:j-1]
 		if kmp(pattern): 
 			ans = max(ans, len(subTree))
 			matchedTree1.append(i)
